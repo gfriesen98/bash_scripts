6be3b4788ac334c02d9a3aaf9a2eef544fa75407
Garett Friesen <garettf999@gmail.com>
2019-12-31 22:18:38 -0600

    Changed font for dmenu_videos

--- dmenu_videos.sh
+++ dmenu_videos.sh
@@ -4,18 +4,18 @@
 # Author: Garett Friesen
 
 main_select_two_electric_boogaloo (){
-  drive=$(df -h | awk '{print $6}' | grep -v "Mounted" | dmenu -l 10 -p "Select mnt'd dir.")
+  drive=$(df -h | awk '{print $6}' | grep -v "Mounted" | dmenu -l 10 -p "Select mnt'd dir." -l 10 -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'GohuNew:bold:pixelsize=16')
   # TODO
   # Add functionality to select a mounted drives path to videos folder
   # folders=$(echo -e "ANIME\nMOVIES" | dmenu -p "Select Type" -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'UbuntuMono Nerd Font Mono:bold:pixelsize=16')
-  folders=$(ls -d ${drive}/* | dmenu -p "Select Folder" -l 10 -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'UbuntuMono Nerd Font Mono:bold:pixelsize=16')
+  folders=$(ls -d ${drive}/* | dmenu -p "Select Folder" -l 10 -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'GohuNew:bold:pixelsize=16')
   if [[ $folders != '' ]]
   then
     #choice=$(echo -e "UNWATCHED\nWATCHED" | dmenu -p "Select Watch Type" -i -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'UbuntuMono Nerd Font Mono:bold:pixelsize=16')
-    choice=$(ls -d ${folders}/* | dmenu -l 10 -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'UbuntuMono Nerd Font Mono:bold:pixelsize=16')
+    choice=$(ls -d ${folders}/* | dmenu -l 10 -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'GohuNew:bold:pixelsize=16')
     if [[ $choice != '' ]]
     then
-      path=$(ls -d ${choice}/* | dmenu -p "🎥 What Video?" -i -l 15 -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'UbuntuMono Nerd Font Mono:bold:pixelsize=16')
+      path=$(ls -d ${choice}/* | dmenu -p "🎥 What Video?" -i -l 15 -nb '#000000' -nf '#bcb1a3' -sb '#1c3b56' -sf '#bcb1a3' -fn 'GohuNew:bold:pixelsize=16')
       if [[ $path != '' ]]
       then 
         notify-send "🎬 ${path} is playing"
